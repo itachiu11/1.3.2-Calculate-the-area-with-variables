a93d96279e8bf2bbfd930448d2c8a17d6918eac8
itachiu <224637143+itachiu11@users.noreply.github.com>
2025-11-17 20:02:04 -0800

    Add area calculation and console output
    Calculate area using height and width inputs.

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ var height = prompt("Enter the height", 150);
 var width = prompt("Enter the width", 230);
     
 // Write your code below
-
+var height = 150
+var width = 230
+area = (height * width);
 
 
 
 // Print out result in the console log statement below 
-console.log();
+console.log(area);
+//34500
+    
